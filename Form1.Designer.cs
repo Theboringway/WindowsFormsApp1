1914cf696084b863e483f2d5315cf08d1d1540c7
The boring  way <eliotoukil@outlook.fr>
2020-03-08 17:29:15 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(131, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 212);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(548, 111);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(208, 212);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(422, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<-\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "supprimer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(432, 422);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "ajouter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(630, 371);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 33);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "modifier";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(422, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "oukkil lzlz",
+            "omar oukil",
+            "safir oukil",
+            "ffr toto"});
+            this.comboBox1.Location = new System.Drawing.Point(376, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ChercherLeMot);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(904, 491);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private object[] ch;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private TextBox textBox1;
+        private object stock;
+        private ComboBox comboBox1;
+    }
+   
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBox2.Items.AddRange(listBox1.Items);
+            listBox1.Items.Clear();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.AddRange(listBox2.Items);
+            listBox2.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+                    listBox1.Items.Remove(textBox1.Text);
+            //refresh the buffer
+                    listBox1.Items.Remove("spiderddken");
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            listBox2.Items.Add(listBox1.SelectedItem);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox2.Items[listBox2.SelectedIndex] = textBox1.Text;
+            
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text =(string) listBox2.SelectedItem;
+        }
+
+        private bool analyse(string tt1, string tt2)
+        {
+            int j = 0;
+            for (int i = 0; i < tt1.Length; i++)
+            {
+
+                foreach (char c in tt2)
+                {
+                    if (i + j <= tt1.Length)
+                        if (c == tt1[i + j])
+                        {
+                            j++;
+                        }
+                        else
+                            j = 0;
+                    if (j == tt2.Length)
+                        return true;
+                }
+                j = 0;
+            }
+            return false;
+        }
+        private void ChercherLeMot(object sender, KeyEventArgs e)
+        {
+            //sauvgarde de la combolist dans un tableau 
+             List<object> toto1 = new List<object>();
+
+             foreach (object ll in comboBox1.Items)
+             {
+                 toto1.Add(ll);
+                comboBox1.Items.Remove(ll);
+            }
+            //vider la combolist
+
+             comboBox1.Items.Remove(toto1);
+
+            //recherche des mot et les inserer dans la combo
+           
+            foreach (object tt in comboBox1.Items)
+            {
+
+                if (analyse(tt.ToString(), comboBox1.Text))
+                {
+                    
+                    //MessageBox.Show(tt);
+                    listBox1.Items.Add(tt);
+                    
+                }
+            }
+        }
+
+        
+    }
+
+
+        
+    }
+
